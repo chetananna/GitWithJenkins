34f4810557bf71ce825915b151c17d52b0ff0529
chetananna21 <chetan.anna@harman.com>
2020-06-11 14:10:08 +0530

    Demo Test commit

---
+++ src/test/java/com/testCases/DemoTest.java
@@ -0,0 +1,12 @@
+package com.testCases;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+    @Test
+	void demoTest(){
+		// TODO Auto-generated method stub
+     System.out.println("Welcome to the Git hub");
+	}
+
+}
