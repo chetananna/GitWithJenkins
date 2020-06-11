835ce01d7e6ee4076a439cfebc26799ab075b625
chetananna21 <chetan.anna@harman.com>
2020-06-11 21:25:21 +0530

    remove the comment

--- src/test/java/com/testCases/DemoTest.java
+++ src/test/java/com/testCases/DemoTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class DemoTest {
     @Test
 	void demoTest(){
-		//this project is about git integration with jenkins
+		
      System.out.println("Welcome to the Git hub");
  System.out.println("will run the build through jenkins");
 	}
